d3c9d9f5fc0ec39f5c1c2c4d6c8f1b606967d18b
YT <filin@ymail.com>
2021-11-23 14:56:36 +0200

    Add new exersize

--- Module2Grade.py
+++ Module2Grade.py
@@ -7,7 +7,7 @@ def format_name(first_name, last_name):
 		string = "Name: " + last_name + "," + first_name
 	elif first_name!="" and last_name == "":
 		string = "Name: " + first_name
-        
+
 	return string 
 
 
@@ -22,3 +22,17 @@ print(format_name("Voltaire", ""))
 
 print(format_name("", ""))
 # Should return an empty string
+
+def fractional_part(numerator, denominator):
+	if denominator == 0 or numerator == 0:
+		return 0
+	else:
+		num = numerator/denominator - numerator//denominator
+	return num
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
